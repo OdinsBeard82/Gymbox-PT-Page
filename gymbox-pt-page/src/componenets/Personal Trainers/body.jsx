971e25c0b69fbe34767b4e5834617ecf301727a3
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-11-09 19:54:23 +0000

    save before bug fix i keep re-inputting npm start

--- gymbox-pt-page/src/componenets/Personal Trainers/body.jsx
+++ gymbox-pt-page/src/componenets/Personal Trainers/body.jsx
@@ -6,7 +6,8 @@ import VPTBoxer from '../img/VPT-Boxer.jpeg';
 function PersonalTrainerHeader() {
   return (
     <div className="pt">
-      <p>PERSONAL TRAINERS</p>
+      <p>CHOOSE YOUR TRAINER</p>
+      <p class="pickTrainer">PICK YOUR TRAINER</p>
       <div className="imagesText">
         <img src={VPTimage} alt="VPTimage" className="trainer-image" />
         <div class='box1'></div>
@@ -15,8 +16,9 @@ function PersonalTrainerHeader() {
           <p>Whether you need help giving gymtimidation the boot or expert programming to smash through those PBs, Gymbox PTs guarantee to give your workouts all the gear AND plenty of ideas. How? Well, cos we’ve trained them.</p>
         </div>
         </div>
+        <div class='box2'>
         <img src={VPTBoxer} alt="VPTBoxer" className="boxer-image" />
-        <div class='box2'></div>
+        </div>
         <div className="text-content2">
           <h3>Why work with a coach?</h3>
           <p>The Gymbox Academy works with some of the best coaches in London – each one bringing their unique personalities, specialist insight and expertise in club upon finishing training. Armed with the sharpest skills and latest techniques, choosing a Gymbox Personal Trainer means that whatever your goal, you’ll be in the safest (and strongest) hands around. Teaming up with your PT, you can expect tailored assessments and programmes to guide you. Access to more knowledge than Google and Stephen Fry combined. And plenty of professional expertise in weight management, strength training, rehabilitation, body composition, combat sports, movement health, and functional training.
